025f73d02af18ca8b6912b4d0f1bded53bf0de1c
Голубова Виктория <viki12007@inbox.ru>
2022-07-14 21:45:11 +0000

    14 module

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+number = int(input('Введите целое положительное число: '))
+
+
+def get_count_numbers(number):
+    return len(str(number))
+
+
+def get_sum_numbers(number):
+    sum = 0
+    for simbol in str(number):
+        sum += int(simbol)
+    return sum
+
+
+count_numbers = get_count_numbers(number)
+sum_numbers = get_sum_numbers(number)
+
+print('Сумма чисел:', sum_numbers)
+print('Количество цифр в числе:', count_numbers)
+print('Разность суммы и количества цифр:', sum_numbers - count_numbers)
--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,23 @@
 # TODO здесь писать код
 
+first = float(input('Введите первое положительное вещественное число: '))
+second = float(input('Введите второе положительное вещественное число: '))
 
+
+def getReverseStr (str):
+    return ''.join(reversed(str))
+
+
+def getReverseNumbers (number):
+    breakNumber = str(number).split('.')
+    reversedBreakNumber = map(getReverseStr, breakNumber)
+    reversedNumber = '.'.join(reversedBreakNumber)
+    return float(reversedNumber)
+
+
+reversedFirst = getReverseNumbers(first)
+reversedSecond = getReverseNumbers(second)
+
+print('Первое число наоборот:', reversedFirst)
+print('Второе число наоборот:', reversedSecond)
+print('Сумма:', reversedFirst + reversedSecond)

a9840d78d2f99611dd4f80135639070ced6e69ec
viki1 <v.golubova@denexy.com>
2022-07-14 16:19:51 +0400

    Сompleted the remaining tasks.

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+
+number = int(input('Введите целое положительное число, которое больше единицы: '))
+
+
+def getMinDivisor (number):
+    if number <= 1:
+        return print('Число должно быть больше 1!')
+    for step in range(2, number + 1):
+        if number % step == 0:
+            return step
+
+
+minDivisior = getMinDivisor(number)
+
+print('Наименьший делитель, отличный от единицы:', minDivisior)

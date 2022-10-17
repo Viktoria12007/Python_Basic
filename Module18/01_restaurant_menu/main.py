ffd76a3418e720f587ad103ac3fa29f16f0ce573
Голубова Виктория <viki12007@inbox.ru>
2022-10-17 18:37:25 +0000

    New task

--- Module18/01_restaurant_menu/main.py
+++ Module18/01_restaurant_menu/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+menu = 'утиное филе;фланк-стейк;банановый пирог;плов'
+print('Доступное меню:', menu)
+menu_arr = menu.split(';')
+new_menu = ', '.join(menu_arr)
+print('На данный момент в меню есть:', new_menu)

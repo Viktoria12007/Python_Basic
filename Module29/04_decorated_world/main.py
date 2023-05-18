547953f4c86e494632aee22667f647b108726f41
Виктория Голубова <v.golubova@denexy.com>
2023-05-18 10:27:50 +0400

    Move the print inside the wrapper function

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -3,25 +3,22 @@ from typing import Callable
 
 
 def decorator_with_args_for_any_decorator(decorator_for_decoration: Callable) -> Callable:
-    @functools.wraps(decorator_for_decoration)
-    def wrapped_(*args, **kwargs):
-        def decorator(func):
+    def decorator_maker(*args, **kwargs) -> Callable:
+        def decorator_wrapper(func: Callable) -> Callable:
             res = decorator_for_decoration(func, *args, **kwargs)
             return res
-        return decorator
-    return wrapped_
+        return decorator_wrapper
+    return decorator_maker
 
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(func: Callable, *args, **kwargs) -> Callable:
-    print(f'Переданные арги и кварги в декоратор: {args}, {kwargs}')
-
+def decorated_decorator(func: Callable, *dec_args, **dec_kwargs) -> Callable:
     @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        print('start wrapped')
-        result = func(*args, **kwargs)
+    def wrapper(*func_args, **func_kwargs) -> Callable:
+        print(f'Переданные арги и кварги в декоратор: {dec_args}, {dec_kwargs}')
+        result = func(*func_args, **func_kwargs)
         return result
-    return wrapped
+    return wrapper
 
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
@@ -30,3 +27,4 @@ def decorated_function(text: str, num: int) -> None:
 
 
 decorated_function("Юзер", 101)
+decorated_function("Юзер2", 102)

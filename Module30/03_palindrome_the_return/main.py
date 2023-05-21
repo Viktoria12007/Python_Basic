347d01d441636b9d67b7b381e1c2955ad6f88b11
Виктория Голубова <v.golubova@denexy.com>
2023-05-21 15:59:22 +0400

    One line solution for 03_palindrome_the_return

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -2,13 +2,7 @@ from collections import Counter
 
 
 def can_be_poly(my_str: str) -> bool:
-    c = Counter(my_str).most_common()
-    count_even_elements = len(list(filter(lambda item: item[1] % 2 == 0, c)))
-    count_odd_elements = len(c) - count_even_elements
-    if (count_even_elements >= 1 and count_odd_elements <= 1) or (count_even_elements == 0 and count_odd_elements == 1)\
-            or (count_even_elements >= 1 and count_odd_elements == 0):
-        return True
-    return False
+    return len(list(filter(lambda count: count % 2, Counter(my_str).values()))) <= 1
 
 
 print(can_be_poly('abcba'))
